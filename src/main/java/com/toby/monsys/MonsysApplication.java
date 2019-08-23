eabac71f110ddda3a113bbd82a83c662423c09f3
mousehaow <52684056@qq.com>
2019-08-23 09:01:33 +0800

    Initial commit

---
+++ src/main/java/com/toby/monsys/MonsysApplication.java
@@ -0,0 +1,13 @@
+package com.toby.monsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonsysApplication.class, args);
+    }
+
+}
